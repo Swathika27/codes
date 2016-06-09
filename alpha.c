1640384c16553d46a12d85bb2a4649c3778dabed
Swathika27 <swafi.vi270@gmail.com>
2016-06-09 15:52:40 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	char a;
+	scanf("%c",&a);
+	if(a=='a'|| a=='e'||a=='i'||a=='o'||a=='u')
+	{
+		printf("vowel");
+		
+	}
+	else
+	{
+		printf("consonant");
+	}
+	return 0;
+}
